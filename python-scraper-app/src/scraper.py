fef91710a6f404b8797c947462575fdde5551d0e
DotHack88 <dothack@live.it>
2025-06-06 23:26:16 +0200

    Aggiorna la versione a 1.1.0 e modifica i tassi di cambio per AUD e NZD

--- python-scraper-app/src/scraper.py
+++ python-scraper-app/src/scraper.py
@@ -65,11 +65,11 @@ EXCHANGE_RATES = {
     "GBP": 1.17,   # Sterlina britannica
     "NOK": 0.087,  # Corona norvegese
     "CHF": 1.04,   # Franco svizzero
-    "AUD": 0.61,   # Dollaro australiano
+    "AUD": 0.57,   # Dollaro australiano
     "CAD": 0.68,   # Dollaro canadese
     "ILS": 0.25,   # Shekel israeliano
     "MXN": 0.054,  # Peso messicano v
-    "NZD": 0.57,   # Dollaro neozelandese
+    "NZD": 0.53,   # Dollaro neozelandese
     "HKD": 0.12,   # Dollaro di Hong Kong
     "JPY": 0.062,   # Yen giapponese
     "USD": 0.92,   # Dollaro USA
@@ -500,7 +500,7 @@ def genera_post_telegram(info):
         post = f'<a href="{info["cover"]}">&#8205;</a>\n' + post  # anteprima immagine
     return post
 
-VERSIONE_CORRENTE = "1.0.2"
+VERSIONE_CORRENTE = "1.1.0"
 URL_VERSIONE = "https://raw.githubusercontent.com/DotHack88/ps-scraper/main/version.txt"
 URL_DOWNLOAD = "https://github.com/DotHack88/ps-scraper/releases/download/v1.0.0/scraper.exe"
 
