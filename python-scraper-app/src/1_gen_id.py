3790d847c01b2c6e13034a0860cc79dc61141fb1
Cloud Strife <40268801+DotHack88@users.noreply.github.com>
2025-06-22 09:22:34 +0200

    Delete python-scraper-app/src directory

--- python-scraper-app/src/1_gen_id.py
+++
@@ -1,9 +0,0 @@
-import uuid
-import hashlib
-
-def get_machine_id():
-    return hashlib.sha256(str(uuid.getnode()).encode()).hexdigest()[:16]
-
-if __name__ == "__main__":
-    machine_id = get_machine_id()
-    print(f"🖥️ Il tuo ID macchina è: {machine_id}")
